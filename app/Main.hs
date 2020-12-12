efb1a50a8d5cb6781e02e2e8c9e2ffbcded2584a
Spencer Brown <spencerb@warpmail.net>
2020-12-11 22:14:36 -0500

    Escape backslash

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ testinput = "123"
 main :: IO ()
 
 main = do
-    putStrLn "Enter an equation. Multiplication is denoted by *. Division can be done with the latex symbol \frac{a}{b} where a and b are expressions. Normal mathematical precedence is assumed. Only accepts equations. Does not solve or simplify trigonometric functions, logarithms, or powers with a variable in the exponent."
+    putStrLn "Enter an equation. Multiplication is denoted by *. Division can be done with the latex symbol \\frac{a}{b} where a and b are expressions. Normal mathematical precedence is assumed. Only accepts equations. Does not solve or simplify trigonometric functions, logarithms, or powers with a variable in the exponent."
     input <- getLine
     let result = parseEquation input
     let (steps, solution) = either (\err -> (show err, Equation BAEUndefined BAEUndefined)) (\eq -> solutionSteps ((simplifyEquation . simplifyEquation) eq)) result
